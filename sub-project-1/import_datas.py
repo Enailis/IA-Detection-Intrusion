99ecdb5edf1ce84d34ab1dff969047d3da073c13
Remag29 <73289897+Remag29@users.noreply.github.com>
2023-10-12 09:24:30 +0200

    Clean code

--- sub-project-1/import_datas.py
+++ sub-project-1/import_datas.py
@@ -12,13 +12,14 @@ def get_dictionnaries():
     PASSWORD = os.getenv("PASSWORD")
 
     # Create ElasticSearch object
-    es = Elasticsearch("https://tdelastic:9200", verify_certs=False, request_timeout=1000000, basic_auth=(LOGIN, PASSWORD))
+    es = Elasticsearch("https://tdelastic:9200", verify_certs=False, request_timeout=1000000,
+                       basic_auth=(LOGIN, PASSWORD))
 
     directory = "TRAIN_ENSIBS"
     dictionnaries = []
 
     print("[+] Indexing file")
-    
+
     # Foreach files in "TRAIN_ENSIBS" directory
     for filename in os.listdir(directory):
         # Filter to check only XML files
@@ -32,8 +33,9 @@ def get_dictionnaries():
 
             # Initialize a list to store flow dictionaries
             flow_data = []
-            flow_elements = root.xpath("//TestbedMonJun14Flows | //TestbedSatJun12 | //TestbedSunJun13Flows | //TestbedThuJun17-1Flows | //TestbedThuJun17-2Flows | //TestbedTueJun15-1Flows | //TestbedTueJun15-2Flows | //TestbedWedJun16-1Flows | //TestbedWedJun16-2Flows")
-            filename = filename.replace('.xml','').replace('Flows','').replace('bis','')
+            flow_elements = root.xpath(
+                "//TestbedMonJun14Flows | //TestbedSatJun12 | //TestbedSunJun13Flows | //TestbedThuJun17-1Flows | //TestbedThuJun17-2Flows | //TestbedTueJun15-1Flows | //TestbedTueJun15-2Flows | //TestbedWedJun16-1Flows | //TestbedWedJun16-2Flows")
+            filename = filename.replace('.xml', '').replace('Flows', '').replace('bis', '')
             # Loop through the flow elements and convert each flow to a dictionary
             for flow_elem in flow_elements:
                 flow_dict = {}
@@ -41,7 +43,8 @@ def get_dictionnaries():
                     if child_elem.tag == "Tag":
                         flow_dict[child_elem.tag] = child_elem.text
                     else:
-                        flow_dict[child_elem.tag] = int(child_elem.text) if child_elem.tag.endswith(("Bytes", "Packets", "Port")) else child_elem.text
+                        flow_dict[child_elem.tag] = int(child_elem.text) if child_elem.tag.endswith(
+                            ("Bytes", "Packets", "Port")) else child_elem.text
                 flow_data.append(flow_dict)
 
             # Index the flow data in Elasticsearch with the origin file information
