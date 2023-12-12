939b1342f63b8d5af634aaecaf10437d7b067e98
Remag29 <73289897+Remag29@users.noreply.github.com>
2023-12-12 12:40:08 +0100

    Add specific folder for challenge1 and 2

--- sub-project-3/challenge1/challenge1.py
+++ sub-project-3/challenge1/challenge1.py
@@ -5,9 +5,12 @@ from sklearn.preprocessing import LabelEncoder
 
 from split_datas import get_pickle_file
 
+# Load train datas #####################################################################################################
+print("[+] Loading train datas")
 datas = get_pickle_file()
 datas_http = pd.DataFrame(datas["HTTPWeb"])
 datas_ssh = pd.DataFrame(datas["SSH"])
+print("[+] Success\n")
 
 x_train = datas_http.drop(columns=["Tag", "origin_file", "startTime"], axis=1)
 y_train = datas_http["Tag"]
@@ -19,14 +22,18 @@ y_train_encoded = label_encoder.fit_transform(y_train)
 
 imputer = SimpleImputer(strategy='mean')
 X_train_imputed = imputer.fit_transform(X_train_encoded)
+########################################################################################################################
 
 # Init KNN
 random_forest_classifier = RandomForestClassifier(n_estimators=100)
 
 # Train model
+print("[+] Training model")
 random_forest_classifier.fit(X_train_imputed, y_train_encoded)
+print("[+] Success\n")
 
-# Load test datas
+# Load test datas ######################################################################################################
+print("[+] Loading test datas")
 from load_http import get_dictionnaries
 datas_http_test = get_dictionnaries()
 datas_http_test = pd.DataFrame(flow['_source'] for flow in datas_http_test)
@@ -41,11 +48,14 @@ y_test_encoded = label_encoder.fit_transform(y_test)
 
 imputer = SimpleImputer(strategy='mean')
 X_test_imputed = imputer.fit_transform(X_test_encoded)
+print("[+] Success\n")
 
-# Predict using test datas
+# Predict using test datas #############################################################################################
+print("[+] Predicting using test datas")
 y_pred = random_forest_classifier.predict(X_test_imputed)
+print("[+] Success\n")
+print(y_pred)
 
-# Calculate accuracy
-from sklearn.metrics import accuracy_score
-accuracy = accuracy_score(y_test_encoded, y_pred)
-print("Accuracy: " + str(accuracy))
+# Get probability of each prediction
+y_pred_proba = random_forest_classifier.predict_proba(X_test_imputed)
+print(y_pred_proba)
---
+++ sub-project-3/challenge1/result_file.py
@@ -0,0 +1,14 @@
+def create_result_file(predictions, probabilities):
+    import json
+    res = dict()
+
+    res['preds'] = list(predictions)  # list of predicted labels
+    res['probs'] = list(probabilities)  # list of probas/scores (probas of attack (class 1))
+
+    res['names'] = ['NAME1', 'NAME2']  # list of team member name(s )
+    res['method'] = METHOD_NAME  # methode name
+    res['appName'] = APP_NAME  # "SSH" ou "HTTPWeb"
+    res['version'] = VERSION  # submission version number
+    f = open("<NAME1>_<NAMES2>_<APP_NAME>_<VERSION>" + ".res", "w")  # VERSION = 1, 2 ou 3
+    f.write(json.dumps(res))
+    f.close()
