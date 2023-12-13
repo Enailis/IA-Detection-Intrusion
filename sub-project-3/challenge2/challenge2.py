7d04f824c2c436fed17b93414ad92c6505dd933d
ENA <enailismail133@gmail.com>
2023-12-13 12:34:36 +0100

    update load_file.py

---
+++ sub-project-3/challenge2/challenge2.py
@@ -0,0 +1,2 @@
+# Test with RandomForestClassifier with max_depth hyperparameter setup
+# Test to fit by splitting the datas in chunks
--- sub-project-3/challenge2/load_file.py
+++ sub-project-3/challenge2/load_file.py
@@ -1,20 +1,23 @@
+import pickle
 from lxml import etree as ET
 
 
-def load_test():
-    file = "data/traffic_os_TEST.xml"
-
+def load_file(file):
     flows = []
-    tree = ET.parse(file)
-    root = tree.getroot()
-
-    flow_elements = root.xpath("FFlow")
-
-    for flow_elem in flow_elements:
+    context = ET.iterparse(file, events=("end",), tag=("FFlow"))
+    
+    for _, elem in context:
         flow_dict = {}
-        for child_elem in flow_elem.getchildren():
-            if child_elem.tag in ["Timestamp", "Duration", "Src_Pt", "Dst_Pt", "Packets", "Bytes", "Flows", "Tos"]:
-                flow_dict[child_elem.tag] = int(child_elem.text)
+        for child_elem in elem.getchildren():
+            if child_elem.tag in ["Timestamp", "Duration", "Src_Pt", "Dst_Pt", "Packets", "Flows", "Tos"]:
+                flow_dict[child_elem.tag] = int(float(child_elem.text.replace(' ', '')))
+            elif child_elem.tag == "Bytes":
+                value = child_elem.text.replace(' ', '')
+                convertion = {("M", '000000'), ("K", '000'), ("G", '000000000')}
+                for k, v in convertion:
+                    if k in value:
+                        value = value.replace(k, v).replace('.', '')
+                flow_dict[child_elem.tag] = int(float(value))
             else:
                 flow_dict[child_elem.tag] = child_elem.text
         flows.append(flow_dict)
@@ -26,26 +29,51 @@ def load_test():
     return df
 
 
-def load_train():
-    file = "data/traffic_os_TRAIN.xml"
+def export_pickle(train, test):
+    with open('train.pickle', 'wb') as handle:
+        pickle.dump(train, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-    flows = []
-    tree = ET.parse(file)
-    root = tree.getroot()
+    print("\n[+] Success")
+    print(" ╰─ " + "The train datas has been serialized")
 
-    flow_elements = root.xpath("FFlow")
+    with open('test.pickle', 'wb') as handle:
+        pickle.dump(test, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-    for flow_elem in flow_elements:
-        flow_dict = {}
-        for child_elem in flow_elem.getchildren():
-            if child_elem.tag in ["Timestamp", "Duration", "Src_Pt", "Dst_Pt", "Packets", "Bytes", "Flows", "Tos"]:
-                flow_dict[child_elem.tag] = int(child_elem.text)
-            else:
-                flow_dict[child_elem.tag] = child_elem.text
-        flows.append(flow_dict)
+    print("\n[+] Success")
+    print(" ╰─ " + "The train datas has been serialized")
 
-    # Transform flow list into pandas dataframe
-    import pandas as pd
-    df = pd.DataFrame(flows)
 
-    return df
+def load_pickle():
+    print("\n[~] Loading pickle files...")
+    with open('train.pickle', 'rb') as handle:
+        train = pickle.load(handle)
+
+    with open('test.pickle', 'rb') as handle:
+        test = pickle.load(handle)
+
+    print("\n[+] Success")
+    print(" ╰─ " + "The files has been loaded successfully")
+
+    return train, test
+
+
+if __name__ == "__main__":
+    print("\n[~] Loading training datas...")
+    train = load_file("data/traffic_os_TRAIN.xml")
+    print("[+] Training datas:")
+    print(train.head())
+
+    print("\n[~] Loading training datas...")
+    test = load_file("data/traffic_os_TEST.xml")
+    print("[+] Testing datas:")
+    print(test.head())
+    
+    print("\n[~] Exporting datas to pickle files...")
+    export_pickle(train, test)
+    train_pickle, test_pickle = load_pickle()
+
+    print("\n[+] Training datas from pickle file:")
+    print(train_pickle.head())
+
+    print("\n[+] Testing datas from pickle file:")
+    print(test_pickle.head())
